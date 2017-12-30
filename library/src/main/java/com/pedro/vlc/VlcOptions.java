0a4eb9512d4df652c5989ed043786bd3072bab3c
pedroSG94 <pedrossgg1994@gmail.com>
2017-12-30 02:35:47 +0100

    vlc 2.5.14

--- library/src/main/java/com/pedro/vlc/VlcOptions.java
+++ library/src/main/java/com/pedro/vlc/VlcOptions.java
@@ -1,65 +1,15 @@
 package com.pedro.vlc;
 
 import java.util.ArrayList;
-import org.videolan.libvlc.util.VLCUtil;
 
 /**
  * Created by pedro on 25/06/17.
  */
 public class VlcOptions {
 
-  private boolean verboseMode = BuildConfig.DEBUG;
-
   public ArrayList<String> getDefaultOptions() {
     ArrayList<String> options = new ArrayList<>();
-    options.add("--no-audio-time-stretch");
-    options.add("--avcodec-skiploopfilter");
-    options.add(String.valueOf(getDeblocking(-1)));
-    options.add("--avcodec-skip-frame");
-    options.add("0");
-    options.add("--avcodec-skip-idct");
-    options.add("0");
-    options.add("--audio-resampler");
-    options.add(getResampler());
-    options.add(verboseMode ? "-vv" : "-v");
-    //delay = network buffer + file buffer
-    //options.add(":file-caching=" + Constants.BUFFER);
-    //options.add(":network-caching=" + Constants.BUFFER);
+    options.add("-vvv");
     return options;
-  }
-
-  /*
-   The below functions are borrowed from the official VLC app:
-   */
-  private int getDeblocking(int deblocking) {
-    if (deblocking < 0) {
-      /*
-       Set some reasonable deblocking defaults:
-       Skip all (4) for armv6 and MIPS by default
-       Skip non-ref (1) for all armv7 more than 1.2 Ghz and more than 2 cores
-       Skip non-key (3) for all devices that don't meet anything above
-       */
-      VLCUtil.MachineSpecs machineSpecs = VLCUtil.getMachineSpecs();
-      if (machineSpecs == null) {
-        return deblocking;
-      } else if ((machineSpecs.hasArmV6 && !(machineSpecs.hasArmV7)) || machineSpecs.hasMips) {
-        return 4;
-      } else if (machineSpecs.frequency >= 1200 && machineSpecs.processors > 2) {
-        return 1;
-      } else if (machineSpecs.bogoMIPS >= 1200 && machineSpecs.processors > 2) {
-        return 1;
-      } else {
-        return 3;
-      }
-    } else if (deblocking > 4) {
-      return 3;
-    } else {
-      return deblocking;
-    }
-  }
-
-  private String getResampler() {
-    VLCUtil.MachineSpecs machineSpecs = VLCUtil.getMachineSpecs();
-    return (machineSpecs == null || machineSpecs.processors > 2) ? "soxr" : "ugly";
   }
 }
--- library/src/main/java/com/pedro/vlc/VlcVideoLibrary.java
+++ library/src/main/java/com/pedro/vlc/VlcVideoLibrary.java
@@ -18,10 +18,10 @@ import org.videolan.libvlc.MediaPlayer;
  */
 public class VlcVideoLibrary implements MediaPlayer.EventListener {
 
+  private int width = 0, height = 0;
   private LibVLC vlcInstance;
   private MediaPlayer player;
   private VlcListener vlcListener;
-  private boolean playing = false;
   //The library will select one of this class for rendering depend of constructor called
   private SurfaceView surfaceView;
   private TextureView textureView;
@@ -62,44 +62,57 @@ public class VlcVideoLibrary implements MediaPlayer.EventListener {
     vlcInstance = new LibVLC(context, new VlcOptions().getDefaultOptions());
   }
 
-  public boolean isPlaying() {
-    return playing;
+  public VlcVideoLibrary(Context context, VlcListener vlcListener, Surface surface, int width,
+      int height) {
+    this.vlcListener = vlcListener;
+    this.surface = surface;
+    this.width = width;
+    this.height = height;
+    surfaceHolder = null;
+    vlcInstance = new LibVLC(context, new VlcOptions().getDefaultOptions());
   }
 
-  public void play(final String endPoint) {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        if (!playing) {
-          playing = true;
-          setMedia(new Media(vlcInstance, Uri.parse(endPoint)));
-        }
-      }
-    }).start();
+  public VlcVideoLibrary(Context context, VlcListener vlcListener, Surface surface,
+      SurfaceHolder surfaceHolder, int width, int height) {
+    this.vlcListener = vlcListener;
+    this.surface = surface;
+    this.surfaceHolder = surfaceHolder;
+    this.width = width;
+    this.height = height;
+    vlcInstance = new LibVLC(context, new VlcOptions().getDefaultOptions());
+  }
+
+  public boolean isPlaying() {
+    return player != null && player.isPlaying();
+  }
+
+  public void play(String endPoint) {
+    if (player == null || player.isReleased()) {
+      setMedia(new Media(vlcInstance, Uri.parse(endPoint)));
+    } else if (!player.isPlaying()) {
+      player.play();
+    }
   }
 
   public void stop() {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        if (playing) {
-          playing = false;
-          player.stop();
-          player.release();
-          player = null;
-        }
-      }
-    }).start();
+    if (player != null && player.isPlaying()) {
+      player.stop();
+      player.release();
+    }
+  }
+
+  public void pause() {
+    if (player != null && player.isPlaying()) {
+      player.pause();
+    }
   }
 
   private void setMedia(Media media) {
     //delay = network buffer + file buffer
     //media.addOption(":network-caching=" + Constants.BUFFER);
     //media.addOption(":file-caching=" + Constants.BUFFER);
-    //media.addOption(":clock-jitter=0");
-    //media.addOption(":clock-synchro=0");
-    //media.addOption(":codec=all");
-
+    media.addOption(":fullscreen");
+    media.setHWDecoderEnabled(true, false);
     player = new MediaPlayer(vlcInstance);
     player.setMedia(media);
     player.setEventListener(this);
@@ -108,8 +121,12 @@ public class VlcVideoLibrary implements MediaPlayer.EventListener {
     //set correct class for render depend of constructor called
     if (surfaceView != null) {
       vlcOut.setVideoView(surfaceView);
+      width = surfaceView.getWidth();
+      height = surfaceView.getHeight();
     } else if (textureView != null) {
       vlcOut.setVideoView(textureView);
+      width = textureView.getWidth();
+      height = textureView.getHeight();
     } else if (surfaceTexture != null) {
       vlcOut.setVideoSurface(surfaceTexture);
     } else if (surface != null) {
@@ -117,6 +134,7 @@ public class VlcVideoLibrary implements MediaPlayer.EventListener {
     } else {
       throw new RuntimeException("You cant set a null render object");
     }
+    if (width != 0 && height != 0) vlcOut.setWindowSize(width, height);
     vlcOut.attachViews();
     player.setVideoTrackEnabled(true);
     player.play();
